0041a317cc42f632110db7ad56eeae53e23d41a0
Fernando Silva <106024845+Fnord484@users.noreply.github.com>
2024-07-13 16:59:45 -0300

    Update 2_visao_entregadores.py

--- pages/2_visao_entregadores.py
+++ pages/2_visao_entregadores.py
@@ -114,7 +114,7 @@ def top_delivers( df, top_asc ):
 #=============================================
 # Importando a base
 #=============================================
-df = pd.read_csv("C:/Users/Fernando/OneDrive/TRAB/repos/data_sets/comunidade_ds/train.csv")
+df = pd.read_csv("data_sets/comunidade_ds/train.csv")
 
 #Create Backup
 df_backup = df.copy()
